218341009f62b56e4a7fcb3742bf2b2c5e56ae7f
Aman-1603 <59969732+Aman-1603@users.noreply.github.com>
2025-11-13 16:01:28 -0500

    Finished Final Tailwind work on the Hero section

--- netflix-clone/src/component/Hero.js
+++ netflix-clone/src/component/Hero.js
@@ -25,17 +25,35 @@ const Hero = () =>{
       );
     }
 
+    const truncate = (str, length) =>{
+      if(!str){
+        return ""
+      }
+
+      return str.length > length ? str.slice(0,length) + '....' : str
+    }
     const{title, backdrop_path,release_date, overview} = movie;
   
     return (
       <div className="w-full h-[550px] lg:h-[850px]">
         <div className="w-full h-full">
-          <div className="absolute w-full h-[550px] bg-gradient-to-r from-black">
-            <img src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
+          <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"/>
+            <img className="w-full h-full object-cover object-top"
+            src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
             alt={title}
             />
+
+            <div className="absolute w-full top-[20%] lg:top-[35%] p-4 md:p-8">
+              <h1 className="text-3xl md:text-6xl font-nsans-bold">{title}</h1>
+              <div className="mt-8 mb-4">
+                <button className="capitalize border bg-gray-300 text-black px-5 py-2">play</button>
+                <button className="capitalize border border-gray-300 py-2 px-5 ml-4">watch later</button>
+              </div>
+              <p className="text-gray-400 text-sm">{release_date}</p>
+              <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-300">{truncate(overview,165)}</p>
+            </div>
             
-          </div>
+          
         </div>
       </div>
     );
--- netflix-clone/src/pages/Home.js
+++ netflix-clone/src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Hero from "../component/Hero";
+import MovieRow from "../component/MovieRow";
+import endpoints from "../Services/movieServices";
 
 const Home = () => {
   return(
     <>
     <Hero/>
+    <MovieRow title="upcoming" url = {endpoints.upcoming}/>
+    <MovieRow title="trending" url = {endpoints.trending}/>
+    <MovieRow title = "top rated" url = {endpoints.topRated}/>
+    <MovieRow title = "comedy" url = {endpoints.comedy}/>
+    <MovieRow title = "popular" url = {endpoints.popular}/>
     </>
   );
 }
